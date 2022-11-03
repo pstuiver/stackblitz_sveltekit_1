011b196ef56a4d089af1d38c5d2d2c50edc9b7f4
pstuiver <pstuiverx@gmail.com>
2022-11-03 14:11:32 +0200

    1 file modified

--- src/lib/icons/SVG_User.svelte
+++ src/lib/icons/SVG_User.svelte
@@ -2,6 +2,7 @@
   export let width="w-4 xs:w-6 sm:w-8";
 </script>
 
-<svg viewBox="0 0 24 24" class={width}>
+<!-- Material Design Icons mdi:account -->
+ <svg viewBox="0 0 24 24" class={width}>
 <title>User Icon</title>
- <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58V19z"/></svg>
+ <path  d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z"/></svg>
